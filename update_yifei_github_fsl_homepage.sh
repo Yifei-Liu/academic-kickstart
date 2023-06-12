1b9e0cf60147f2782629d5c5895cfba8c6712a8d
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-06-12 15:06:54 -0400

    rename script

--- update_yifei_github_fsl_homepage.sh
+++ update_yifei_github_fsl_homepage.sh
@@ -4,5 +4,7 @@ cd ~/yifei_homepage/academic-kickstart
 git pull
 rm -r ./public/
 rm -r ~/public_html/*
+
+# Update FSL Homepage
 /home/yifei/.linuxbrew/Cellar/hugo/0.63.2/bin/hugo -b "http://www.fsl.cs.sunysb.edu/~yifei/"
 rsync -zavh ~/yifei_homepage/academic-kickstart/public/ ~/public_html/
