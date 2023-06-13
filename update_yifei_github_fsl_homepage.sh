4e2c65697f2045f1d7f7ab8dfaab667f9984615c
Yifei Liu <yifeliu@cs.stonybrook.edu>
2023-06-13 15:17:32 -0400

    Full workflow in update_yifei_github_fsl_homepage.sh

--- update_yifei_github_fsl_homepage.sh
+++ update_yifei_github_fsl_homepage.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
+set -e
 
+# This script should be run on the trex machine
 cd ~/yifei_homepage/academic-kickstart
 git pull
 
 # Update FSL Homepage
+
+# Remove ./public directory
+rm -r ./public/
+
+# Initialize and update all submodules recursively within the Git repo
+git submodule update --init --recursive
 /home/yifei/.linuxbrew/Cellar/hugo/0.63.2/bin/hugo -b "http://www.fsl.cs.sunysb.edu/~yifei/"
+
 rm -r ~/public_html/*
 rsync -zavh ~/yifei_homepage/academic-kickstart/public/ ~/public_html/
+
+# Update github.io Homepage
+rm -r ./public/
+git submodule update --init --recursive
+
+cd public
+
+git checkout main
+git pull
+
+cd ..
+
+/home/yifei/.linuxbrew/Cellar/hugo/0.63.2/bin/hugo
+
+cd public
+
+# Push static html files to the production repo
+git add .
+git commit -m 'Update homepage'
+git push origin main
